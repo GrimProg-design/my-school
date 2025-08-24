327ae1de4c89a36581c41887cb72421e38fe9506
GrimProg <vavilovila01@gmail.com>
2025-08-25 01:46:52 +0600

    Добавил базу данных

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,30 @@ const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // * Подключаем cors * //
 app.use(cors());
 // Ограничиваем доступ только для нашего фронта
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 //  _______________  //
+
+// * Подключаем базу данных * //
+mongoose.connect("mongodb://127.0.0.1:27017/for-school", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('База успешно подключена'))
+.catch(err => console.log(err))
+// * ______________ * //
 
 app.use(express.json());
 app.use("/", indexRoutes);
-app.use('/get', indexRoutes)
-app.use('/post', indexRoutes)
+app.use("/get", indexRoutes);
+app.use("/post", indexRoutes);
 
 module.exports = app;
