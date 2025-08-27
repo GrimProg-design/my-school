c689d84276796a75d506688de4f853a5a4b1bcca
GrimProg <vavilovila01@gmail.com>
2025-08-27 20:50:49 +0600

    Изменил заголовок

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const corsOption = {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    methods: ["GET"],
     allowedApplication: ["Content-Type"]
 }
 
